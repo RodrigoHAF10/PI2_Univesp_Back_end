1ae461e45eeda51fecea1cef2e909f9114e6881d
RodrigoHAF10 <rodrigohaf10@gmail.com>
2024-11-02 14:47:19 -0300

    Ajustes nas views de login e templates; adicionadas novas imagens.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
+from .models import Usuario
 from django.contrib.auth.models import User
-# Register your models here.
 
-#User.objects.create_user('prefeitura1', password='1234')
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ['register_prefeitura', 'coluna_1234']  # Usuário: Prefeitura1 e senha: 1234
+
+# Verifica se o usuário 'prefeitura1' já existe antes de criá-lo
+if not User.objects.filter(username='prefeitura1').exists():
+    User.objects.create_user('prefeitura1', password='1234')
